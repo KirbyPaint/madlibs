6af0174c00fa599ee1b895911bbd1ed4ab37fb35
Ashley Porter <porter.ashley13@gmail.com>
2021-01-19 14:08:40 -0800

    refactor code
    Michael Burton <micbur1@gmail.com>
    Daniel Adeyemi <adeyemidany@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput = $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    inputs = ["name", "second name", "animal's name", "exclamation", "a verb", "a noun"]
+    let answers = [];
+
+    inputs.forEach(function(input) {
+      let user = prompt("Please input " + input);
+      answers.push(user);
+    });
+
+    /*const person1Input = prompt("Please enter your name");
+    const person2Input = prompt("Please enter another name");
+    const animalInput = prompt("Please enter an animal's name");
+    const exclamationInput = prompt("Please enter an exclamation");
+    const verbInput = prompt("Please enter a verb");
+    const nounInput = prompt("Please enter a noun"); */
+
+
+
+    $(".person1").text(answers[0]);
+    $(".person2").text(answers[1]);
+    $(".animal").text(answers[2]);
+    $(".exclamation").text(answers[3]);
+    $(".verb").text(answers[4]);
+    $(".noun").text(answers[5]);
 
     $("#story").show();
   });
